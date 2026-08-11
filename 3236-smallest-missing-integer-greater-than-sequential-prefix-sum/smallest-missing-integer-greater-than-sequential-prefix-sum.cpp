7add75cf5b7ec19d575467ecc437436e18876e74
amrita759 <amritaprajapati204@gmail.com>
2026-08-11 22:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (77.12%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        
+        int sum = nums[0];
+
+        // Find sum of consecutive prefix
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            }
+            else {
+                break;
+            }
+        }
+
+        // Find the smallest number >= sum
+        // that is not present in nums
+        while(true) {
+            
+            int found = 0;
+
+            for(int i = 0; i < nums.size(); i++) {
+                if(nums[i] == sum) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            if(found == 0) {
+                return sum;
+            }
+
+            sum++;
+        }
+    }
+};
